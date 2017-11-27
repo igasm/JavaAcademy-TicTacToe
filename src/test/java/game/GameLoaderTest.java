2b645e8d3cf6f3d8745d48b8ee47101816180268
iga <iga.smarz@gmail.com>
2017-11-27 17:07:50 +0100

    tests refactoring

--- src/test/java/game/GameLoaderTest.java
+++ src/test/java/game/GameLoaderTest.java
@@ -3,49 +3,55 @@ package game;
 import io.ConsoleReader;
 import io.WriterBuilder;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import settings.BoardDimensions;
 import settings.Settings;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class GameLoaderTest {
+
+    GameLoader gameLoader;
+
+    @BeforeTest
+    public void beforeTest(){
+        gameLoader = new GameLoader(
+                new WriterBuilder().byDefault(),
+                new ConsoleReader(),
+                new Settings(new BoardDimensions(3, 3), 3)
+        );
+    }
 
     @DataProvider(name = "dataProviderForMarkTypeValidation")
     public Object[][] provideDataForMarkTypeValidation(){
-        Map<String, MarkType> map = new HashMap<>();
-        map.put("x", MarkType.CROSS);
-        map.put("o", MarkType.NAUGHT);
-        map.put("X", MarkType.CROSS);
-        map.put("O", MarkType.NAUGHT);
-        map.put("", null);
-        map.put(" ", null);
-        map.put("\t", null);
-        map.put("\n", null);
-        map.put("\r", null);
-        map.put(System.getProperty("line.separator"), null);
-        map.put("*", null);
-        map.put("?", null);
-        map.put("\\", null);
-        map.put("-", null);
-        map.put("_", null);
-        map.put("t", null);
-        map.put("T", null);
-        map.put("aa", null);
-        map.put("abecadło", null);
-        map.put("1", null);
-        map.put("1234", null);
-        return new Object[][] {{map}};
+        return new Object[][] {
+                {"x", MarkType.CROSS},
+                {"o", MarkType.NAUGHT},
+                {"X", MarkType.CROSS},
+                {"O", MarkType.NAUGHT},
+                {"", null},
+                {" ", null},
+                {"\t", null},
+                {"\n", null},
+                {"\r", null},
+                {System.getProperty("line.separator"), null},
+                {"*", null},
+                {"?", null},
+                {"\\", null},
+                {"-", null},
+                {"_", null},
+                {"t", null},
+                {"T", null},
+                {"aa", null},
+                {"abecadło", null},
+                {"1", null},
+                {"1234", null}
+        };
     }
 
     @Test(dataProvider = "dataProviderForMarkTypeValidation")
-    public void givenStringForMarkType_expectedMarkType(Map<String, MarkType> map){
-        GameLoader gameLoader = new GameLoader(new WriterBuilder().byDefault(), new ConsoleReader(), new Settings(new BoardDimensions(3, 3), 3));
-        for(Map.Entry<String, MarkType> entry : map.entrySet()){
-            Assert.assertEquals(gameLoader.getMarkType(entry.getKey()), entry.getValue());
-        }
+    public void givenStringForMarkType_expectedMarkType(String markString, MarkType markType){
+        Assert.assertEquals(gameLoader.getMarkType(markString), markType);
     }
 
 }
--- src/test/java/settings/SettingsValidatorTest.java
+++ src/test/java/settings/SettingsValidatorTest.java
@@ -11,92 +11,88 @@ public class SettingsValidatorTest {
 
     @DataProvider(name = "dataProviderForBordDimensionValidation")
     public Object[][] provideDataForBoardDimensionValidation(){
-        Map<Integer, Boolean> map = new HashMap<>();
-        map.put(-1000, false);
-        map.put(-100, false);
-        map.put(-10, false);
-        map.put(-1, false);
-        map.put(0, false);
-        map.put(1, false);
-        map.put(2, false);
-        map.put(3, true);
-        map.put(10, true);
-        map.put(50, true);
-        map.put(100, true);
-        map.put(101, true);
-        map.put(102, false);
-        map.put(110, false);
-        map.put(500, false);
-        map.put(1000, false);
-        return new Object[][] {{map}};
+        return new Object[][]{
+            {-1000, false},
+            {-100, false},
+            {-10, false},
+            {-1, false},
+            {0, false},
+            {1, false},
+            {2, false},
+            {3, true},
+            {10, true},
+            {50, true},
+            {100, true},
+            {101, true},
+            {102, false},
+            {110, false},
+            {500, false},
+            {1000, false}
+        };
     }
 
     @Test(dataProvider = "dataProviderForBordDimensionValidation")
-    public void given_minBorderDimension3_and_maxBorderDimension101_testBoardDimensionValidation(Map<Integer, Boolean> map){
+    public void given_minBorderDimension3_and_maxBorderDimension101_testBoardDimensionValidation(Integer borderDimension, Boolean isValid){
         SettingsValidator settingsValidator = new SettingsValidator(3, 101, 3);
-        for(Map.Entry<Integer, Boolean> entry : map.entrySet()){
-            Assert.assertEquals(settingsValidator.validBorderDimension(entry.getKey()), entry.getValue().booleanValue());
-        }
+        Assert.assertEquals(settingsValidator.validBorderDimension(borderDimension), isValid.booleanValue());
     }
 
     @DataProvider(name = "dataProviderForWinningConditionValidation")
     public Object[][] provideDataForWinningConditionValidation(){
-        Map<Settings, Boolean> map = new HashMap<>();
-        map.put(new Settings(new BoardDimensions(-100, -100), -100), false);
-        map.put(new Settings(new BoardDimensions(0, 0), 0), false);
-        map.put(new Settings(new BoardDimensions(3, 3), -10), false);
-        map.put(new Settings(new BoardDimensions(3, 3), -1), false);
-        map.put(new Settings(new BoardDimensions(3, 3), 0), false);
-        map.put(new Settings(new BoardDimensions(3, 3), 1), false);
 
-        map.put(new Settings(new BoardDimensions(3, 3), 2), false);
-        map.put(new Settings(new BoardDimensions(3, 3), 3), true);
-        map.put(new Settings(new BoardDimensions(3, 3), 4), false);
-        map.put(new Settings(new BoardDimensions(3, 3), 10), false);
-        map.put(new Settings(new BoardDimensions(3, 5), 3), true);
-        map.put(new Settings(new BoardDimensions(3, 5), 5), false);
-        map.put(new Settings(new BoardDimensions(3, 5), 15), false);
+        return new Object[][]{
+        {new Settings(new BoardDimensions(-100, -100), -100), false},
+        {new Settings(new BoardDimensions(0, 0), 0), false},
+        {new Settings(new BoardDimensions(3, 3), -10), false},
+        {new Settings(new BoardDimensions(3, 3), -1), false},
+        {new Settings(new BoardDimensions(3, 3), 0), false},
+        {new Settings(new BoardDimensions(3, 3), 1), false},
 
-        map.put(new Settings(new BoardDimensions(10, 10), 1), false);
-        map.put(new Settings(new BoardDimensions(10, 10), 2), false);
-        map.put(new Settings(new BoardDimensions(10, 10), 3), true);
-        map.put(new Settings(new BoardDimensions(10, 10), 5), true);
-        map.put(new Settings(new BoardDimensions(10, 10), 10), true);
-        map.put(new Settings(new BoardDimensions(10, 10), 15), false);
+        {new Settings(new BoardDimensions(3, 3), 2), false},
+        {new Settings(new BoardDimensions(3, 3), 3), true},
+        {new Settings(new BoardDimensions(3, 3), 4), false},
+        {new Settings(new BoardDimensions(3, 3), 10), false},
+        {new Settings(new BoardDimensions(3, 5), 3), true},
+        {new Settings(new BoardDimensions(3, 5), 5), false},
+        {new Settings(new BoardDimensions(3, 5), 15), false},
 
-        map.put(new Settings(new BoardDimensions(100, 100), -100), false);
-        map.put(new Settings(new BoardDimensions(100, 100), 0), false);
-        map.put(new Settings(new BoardDimensions(100, 100), 1), false);
-        map.put(new Settings(new BoardDimensions(100, 100), 2), false);
-        map.put(new Settings(new BoardDimensions(100, 100), 3), true);
-        map.put(new Settings(new BoardDimensions(100, 100), 5), true);
-        map.put(new Settings(new BoardDimensions(100, 100), 50), true);
-        map.put(new Settings(new BoardDimensions(100, 100), 100), true);
-        map.put(new Settings(new BoardDimensions(100, 100), 101), false);
-        map.put(new Settings(new BoardDimensions(100, 100), 150), false);
+        {new Settings(new BoardDimensions(10, 10), 1), false},
+        {new Settings(new BoardDimensions(10, 10), 2), false},
+        {new Settings(new BoardDimensions(10, 10), 3), true},
+        {new Settings(new BoardDimensions(10, 10), 5), true},
+        {new Settings(new BoardDimensions(10, 10), 10), true},
+        {new Settings(new BoardDimensions(10, 10), 15), false},
 
-        map.put(new Settings(new BoardDimensions(100, 50), -10), false);
-        map.put(new Settings(new BoardDimensions(100, 50), -1), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 0), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 1), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 2), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 3), true);
-        map.put(new Settings(new BoardDimensions(100, 50), 10), true);
-        map.put(new Settings(new BoardDimensions(100, 50), 50), true);
-        map.put(new Settings(new BoardDimensions(100, 50), 55), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 100), false);
-        map.put(new Settings(new BoardDimensions(100, 50), 150), false);
+        {new Settings(new BoardDimensions(100, 100), -100), false},
+        {new Settings(new BoardDimensions(100, 100), 0), false},
+        {new Settings(new BoardDimensions(100, 100), 1), false},
+        {new Settings(new BoardDimensions(100, 100), 2), false},
+        {new Settings(new BoardDimensions(100, 100), 3), true},
+        {new Settings(new BoardDimensions(100, 100), 5), true},
+        {new Settings(new BoardDimensions(100, 100), 50), true},
+        {new Settings(new BoardDimensions(100, 100), 100), true},
+        {new Settings(new BoardDimensions(100, 100), 101), false},
+        {new Settings(new BoardDimensions(100, 100), 150), false},
 
-        return new Object[][] {{map}};
+        {new Settings(new BoardDimensions(100, 50), -10), false},
+        {new Settings(new BoardDimensions(100, 50), -1), false},
+        {new Settings(new BoardDimensions(100, 50), 0), false},
+        {new Settings(new BoardDimensions(100, 50), 1), false},
+        {new Settings(new BoardDimensions(100, 50), 2), false},
+        {new Settings(new BoardDimensions(100, 50), 3), true},
+        {new Settings(new BoardDimensions(100, 50), 10), true},
+        {new Settings(new BoardDimensions(100, 50), 50), true},
+        {new Settings(new BoardDimensions(100, 50), 55), false},
+        {new Settings(new BoardDimensions(100, 50), 100), false},
+        {new Settings(new BoardDimensions(100, 50), 150), false}
+        };
+       
+        
     }
 
     @Test(dataProvider = "dataProviderForWinningConditionValidation")
-    public void given_minBorderDimension3_and_maxBorderDimension101_and_minWinningCondition3_winningConditionValidation(Map<Settings, Boolean> map){
+    public void given_minBorderDimension3_and_maxBorderDimension101_and_minWinningCondition3_winningConditionValidation(Settings settings, boolean isWinningconditionProper){
         SettingsValidator settingsValidator = new SettingsValidator(3, 101, 3);
-        Settings settings;
-        for(Map.Entry<Settings, Boolean> entry : map.entrySet()){
-            settings = entry.getKey();
-            Assert.assertEquals(settingsValidator.validWinningCondition(settings.getBoardDimensions(), settings.getWinningCondition()), entry.getValue().booleanValue());
-        }
+        Assert.assertEquals(settingsValidator.validWinningCondition(settings.getBoardDimensions(), settings.getWinningCondition()), isWinningconditionProper);
     }
 }
